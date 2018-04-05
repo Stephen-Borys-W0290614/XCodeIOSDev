fcf8e05b4b22dfa427bc2de757256f46d94f11d3
user138841 <user138841@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-04-05 14:32:48 -0400

    Initial Commit

---
+++ HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by user138841 on 4/5/18.
+//  Copyright © 2018 user138841. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
